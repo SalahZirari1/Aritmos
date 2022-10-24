93bed4a863436ad312cfae46d03941db9d490f26
SalahEddine-Zirari <108852277+SalahEddine-Zirari@users.noreply.github.com>
2022-10-24 20:52:54 +0100

    coumita

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,13 +1,13 @@
 import { type } from "@testing-library/user-event/dist/type";
-import React, { memo, useState } from "react";
+import React, {useState } from "react";
 import { useEffect } from "react";
 import Buttons from "./Components/Buttons";
 
 export default function Calculator() {
 
     const [inputText, setInputText] = useState("");
-    const [numberEntered, setNumberEntered] = useState([]);
-    const [lastClicked, setLastClicked] = useState("");
+    const [numbersEnteredArray, setNumbersEnteredArray] = useState([]);
+    const [lastClickedClass, setLastClickedClass] = useState("");
     const [currentNumber, setCurrentNumebr] = useState("");
     const [result, setResult] = useState(0);
     const [memory, setMemory] = useState([]);
@@ -17,28 +17,27 @@ export default function Calculator() {
         const textContent = event.target.textContent;
         const className = event.target.className;
 
-        if (lastClicked === "operation-button" && className === "operation-button" && textContent !="C") return;
+        if (lastClickedClass === "operation-button" && className === "operation-button" && textContent !=="C") return;
         if (textContent==="C") {reset();return;}
 
 
         setInputText(prev => prev + textContent);
-        setCurrentNumebr(prev => className !== "operation-button" ? prev + textContent : prev)
-        setNumberEntered(prev => className === "operation-button" ? [...prev, currentNumber] : [...prev])
-        setLastClicked(className)
+        setCurrentNumebr(prev => className !== "operation-button" ? parseFloat(textContent) : prev)
+        setNumbersEnteredArray(prev => className === "operation-button" ? [...prev, currentNumber] : [...prev])
+        setLastClickedClass(className)
         setMemory([className, textContent, currentNumber])
     }
-    console.log(parseFloat(currentNumber))
-    if(result===0) setResult(parseFloat(currentNumber)) // goal is to set result to the first number entred and solve current number NAN error
-    //console.log(memory[2])
-    if (numberEntered.length > 1 && memory[0] === "operation-button") calculate(memory[1])
+    console.log(currentNumber)
+
+    useEffect( ()=> setResult(parseFloat(currentNumber===""?0:currentNumber)) ,[])
+
+    if (numbersEnteredArray.length > 1 && memory[0] === "operation-button") calculate(memory[1])
 
     function calculate(operation) {
-
         switch (operation) {
             case " + ":
                 
                 var newNum = parseFloat(memory[2])
-                //console.log(result)
                 setResult(prev => prev + newNum)
                 setMemory([])
                 
@@ -48,19 +47,19 @@ export default function Calculator() {
 
     function reset(){
         setInputText("")
-        setNumberEntered([])
-        setLastClicked("")
+        setNumbersEnteredArray([])
+        setLastClickedClass("")
         setCurrentNumebr("")
         setResult(0)
         setMemory([])
     }
-
+    //console.log(result)
     return (
         <div className="calculator-div">
 
             <div id="display-div">
                 <div id="operation-div">{inputText}</div>
-                <div id="result-div">{result}</div>
+                <div id="result-div">{isNaN(result)?0:result}</div>
             </div>
             <Buttons onClick={handleClick} />
         </div>
