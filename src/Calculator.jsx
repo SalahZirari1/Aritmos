bf5071d7748f16d05eec56ed1addee8479e7b677
SalahEddine Zirari <muginosezof@outlook.fr>
2022-11-11 19:25:25 +0100

    aaaa

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -18,26 +18,30 @@ export default function Calculator() {
 
         if (lastClickedClass === "operation-button" && className === "operation-button" && textContent !=="C") return;
         if (textContent==="C") {reset();return;}
-
-
+        
         setInputText(prev => prev + textContent);
         setCurrentNumebr(prev => className !== "operation-button" ? parseFloat(prev+textContent) : "") 
         setNumbersEnteredArray(prev => className === "operation-button" ? [...prev, currentNumber] : [...prev])
         setLastClickedClass(className)
         setMemory([className, textContent, currentNumber]);
-
+        
     }
-   
-    // fix first num rendering
-    if (numbersEnteredArray.length > 1 && memory[0] === "operation-button") {setResult(memory[2]);calculate(memory[1])}
     
-    console.log("NumbersEntered:"+numbersEnteredArray)
-    console.log("prevResult:"+result)
-    console.log("currentNumber:"+memory[2])
+    //DONT TOUCH THIS 
+    //without this the first num entered doesnt count i.e 2+3=3
+    if (memory[0] === "operation-button" && numbersEnteredArray.length==1) {setResult(numbersEnteredArray[0]);setMemory([]);}
+    
+    //triggers the calculation method when there are two numbers
+    if (numbersEnteredArray.length > 1 && memory[0] === "operation-button") calculate(memory[1])
 
+    //console.log("NumbersEntered:"+numbersEnteredArray)
+    //console.log("prevResult:"+result)
+/*     console.log("currentNumber:"+memory[2]) 
+ */
     function calculate(operation) {
         switch (operation) {
             case " + ":
+                
                 var newNum = parseFloat(memory[2])
                 setResult(prev => prev + newNum)
                 numbersEnteredArray.length>1 ? setInputText(result+newNum+" + ") : setInputText(result+newNum)
@@ -63,8 +67,8 @@ export default function Calculator() {
 }
     return (
         <div className="calculator-div">
-
-            <div id="display-div">
+{/*             {console.log("low res:"+result)}
+ */}            <div id="display-div">
                 <div id="operation-div">{isNaN(result)?0:result}</div>
                 <div id="result-div">{inputText}</div>
             </div>
