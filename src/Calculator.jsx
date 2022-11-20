f70a0c08890d19208b526ad20e4c26066e48a7c6
SalahEddine Zirari <muginosezof@outlook.fr>
2022-11-20 22:00:16 +0100

    almost there

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,86 +1,106 @@
-import React, {memo, useState } from "react";
+import React, { memo, useState } from "react";
 import Buttons from "./Components/Buttons";
 
 export default function Calculator() {
 
     const [inputText, setInputText] = useState("");
     const [numbersEnteredArray, setNumbersEnteredArray] = useState([]);
-    const [lastClickedClass, setLastClickedClass] = useState(); 
+    const [lastClickedOp, setlastClickedOp] = useState([]);
     const [currentNumber, setCurrentNumebr] = useState("");
     const [result, setResult] = useState(0);
     const [memory, setMemory] = useState([]);
-    const [operationsEntered,setOperationsEntered] = useState([]);
+    const [operationsEntered, setOperationsEntered] = useState([]);
 
 
 
-    // needs to remember last operation entered
-    function handleClick(event) { 
+    function handleClick(event) {
 
         const textContent = event.target.textContent;
         const className = event.target.className;
-        
-        
-        //console.log(lastClickedClass)        
-        //if (memory[0] === "operation-button" && className === "operation-button" && textContent !=="C") return; //need to fix subsequent operaion buttons
-        if (textContent==="C") {reset();return;}
-        
-        setInputText(prev => prev + textContent);
-        setCurrentNumebr(prev => className !== "operation-button" ? parseFloat(prev+textContent) : "") 
+
+
+        if (textContent === "C") { reset(); return; }
+        if(OperationChanged(className, textContent)) {console.log("In"); return;}
+
+        setInputText(prev => prev + textContent); //3+ // [-,+]
+        setCurrentNumebr(prev => className !== "operation-button" ? parseFloat(prev + textContent) : "")
         setNumbersEnteredArray(prev => className === "operation-button" ? [...prev, currentNumber] : [...prev])
-        setOperationsEntered(prev=> {
-            if(className==="operation-button" && prev.length>0)
-            {
-                if(prev.length>1) return prev=[prev[1],textContent]
-                else return [prev[0],textContent]
+        setOperationsEntered(prev => {
+            if (className === "operation-button" && prev.length > 0) {
+                if (prev.length > 1) return prev = [prev[1], textContent]
+                else return [prev[0], textContent]
             }
-            else if(className==="operation-button") return [textContent]
-            else if(className!=="operation-button") return prev
+            else if (className === "operation-button") return [textContent]
+            else if (className !== "operation-button") return prev
             else return []
         })
-        setLastClickedClass(className)
-        setMemory([className, textContent, currentNumber]); 
+        setlastClickedOp([className, textContent])
+        setMemory([className, textContent, currentNumber]);
     }
-    
+
+    //handles consequetive operations clicks
+    function OperationChanged(className, textContent) {
+        if (lastClickedOp[0] === "operation-button" && className === "operation-button" && textContent !== "C") {
+            console.log("in2")
+            if (textContent !== lastClickedOp[1]) {setInputText(result + textContent); setOperationsEntered(prev=>[prev[0],textContent])}
+            return true;
+        }
+        return false;
+    }
 
     //without this the first num entered doesnt count i.e 2+3=3
-    if (memory[0] === "operation-button" && numbersEnteredArray.length===1) {setResult(numbersEnteredArray[0]);setMemory([]);}
-    
-    //triggers the calculation method when there are two numbers
-    if (numbersEnteredArray.length > 1 && memory[0] === "operation-button") calculate(operationsEntered[0],operationsEntered[1])
+    if (memory[0] === "operation-button" && numbersEnteredArray.length === 1) { setResult(numbersEnteredArray[0]); setMemory([]); }
 
-    
-    function calculate(operation,displayOp) {
-        switch (operation) {
+    //triggers the calculation method when there are two numbers
+    if (numbersEnteredArray.length > 1 && memory[0] === "operation-button") calculate(operationsEntered[0], operationsEntered[1])
+
+
+    function calculate(operation, displayOp) {
+       
+        switch (operation,displayOp) {
             case " + ":
-                
                 var newNum = parseFloat(memory[2])
                 setResult(prev => prev + newNum)
-                numbersEnteredArray.length>1 ? setInputText(result+newNum+displayOp) : setInputText(result+newNum)
-                setMemory([])                
+                numbersEnteredArray.length > 1 ? setInputText(result + newNum + displayOp) : setInputText(result + newNum)
+                setMemory([])
                 break;
-            
+
             case " - ":
                 var newNum = parseFloat(memory[2])
                 setResult(prev => prev - newNum)
-                numbersEnteredArray.length>1 ? setInputText(result-newNum+displayOp) : setInputText(result-newNum)
-                setMemory([])                
+                numbersEnteredArray.length > 1 ? setInputText(result - newNum + displayOp) : setInputText(result - newNum)
+                setMemory([])
                 break;
+            case " / ":
+                var newNum = parseFloat(memory[2])
+                setResult(prev => prev / newNum)
+                numbersEnteredArray.length > 1 ? setInputText(result / newNum + displayOp) : setInputText(result / newNum)
+                setMemory([])
+                break;
+            case " x ":
+                var newNum = parseFloat(memory[2])
+                setResult(prev => prev * newNum)
+                numbersEnteredArray.length > 1 ? setInputText(result * newNum + displayOp) : setInputText(result * newNum)
+                setMemory([])
+                break;
+            case " = ":
+                calculate(operation,"")
             default:
-            }
+        }
     }
 
-    function reset(){
+    function reset() {
         setInputText("")
         setNumbersEnteredArray([])
-        setLastClickedClass("")
+        setlastClickedOp("")
         setCurrentNumebr("")
         setResult(0)
         setMemory([])
-}
+    }
     return (
         <div className="calculator-div">
-           <div id="display-div">
-                <div id="operation-div">{isNaN(result)?0:result}</div>
+            <div id="display-div">
+                <div id="operation-div">{isNaN(result) ? 0 : result}</div>
                 <div id="result-div">{inputText}</div>
             </div>
             <Buttons onClick={handleClick} />
@@ -88,6 +108,3 @@ export default function Calculator() {
     )
 }
 
-//todo:
-// line 52: refractor switches into functions
-// line 8:  refractor hook
